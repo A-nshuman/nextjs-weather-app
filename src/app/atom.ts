1e5b0f916ee52e01531c837ca78a444e797e5424
Anshuman Bhardwaj <64587347+A-nshuman@users.noreply.github.com>
2024-05-10 09:24:39 +0300

    Delete src directory

--- src/app/atom.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from "jotai";
-
-export const placeAtom = atom("India");
-
-export const loadingCityAtom = atom(false);
--- src/utils/convertKelvin.ts
+++
@@ -1,4 +0,0 @@
-export function convertKelvinToCelsius(kelvin: number): number {
-    const celcius = kelvin - 273.15
-    return Math.floor(celcius)
-}
