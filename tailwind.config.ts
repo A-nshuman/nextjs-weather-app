dbd1da778bf9b08bcf6d91f8ff87a2ed68847869
Anshuman Bhardwaj <64587347+A-nshuman@users.noreply.github.com>
2024-05-18 09:57:48 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,8 @@ import { transform } from "typescript";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./styles**/*.{css}"
   ],
   theme: {
     extend: {
